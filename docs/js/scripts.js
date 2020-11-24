016e5e5a93166c90959174b8f2d0edb3030b94f5
Andrew Kiang <tsupernet@protonmail.com>
2020-11-23 22:37:55 -0500

    udpate links

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -73,9 +73,9 @@
         handleFetch(`${otherSite}/getJSON`, dataWrapperJSON)
     })
     JSONPnosupport.addEventListener('click', () => {
-        handleJSONP(`${otherSite}/getJSON`, dataWrapperJSONPnosupport)
+        handleJSONP(`https://othersite.netlify.app/data.json`, dataWrapperJSONPnosupport)
     })
     JSONPsupport.addEventListener('click', () => {
-        handleJSONP(`${otherSite}/getJSONP`, dataWrapperJSONPsupport)
+        handleJSONP(`https://othersite.netlify.app/data1.json`, dataWrapperJSONPsupport)
     })
 })()
